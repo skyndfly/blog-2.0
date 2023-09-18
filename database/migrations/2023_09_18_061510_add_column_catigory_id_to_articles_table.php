72cc184e789ad7887f1834a72723a1c946d6ed90
Sergey Golubev <skyandy2796@yandex.ru>
2023-09-18 09:17:48 +0300

    Добавил поле категории в таблицу статей

---
+++ database/migrations/2023_09_18_061510_add_column_catigory_id_to_articles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->foreignIdFor(\App\Models\Category::class)
+                ->constrained()
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropForeign('category_id');
+        });
+    }
+};
