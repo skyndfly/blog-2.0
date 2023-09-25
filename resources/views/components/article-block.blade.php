58498f5ec25fd9f636f8a80c6b07ce3e2761a18f
Sergey Golubev <skyandy2796@yandex.ru>
2023-09-25 17:18:15 +0300

    Переделал вывод категорий на главных страницах с использованием компонентов

---
+++ resources/views/components/article-block.blade.php
@@ -0,0 +1,33 @@
+<div class="col-md-8 flex-column">
+    <a href="{{ route('articles.show',$articles->id) }}" class="card flex-grow-1">
+        <div class="card-head">
+            <div class="category">
+                @if( $articles->category->parentCategory)
+                    <span
+                        class="badge bg-secondary-light">{{ $articles->category->parentCategory->name}}</span>
+                @endif
+                <span
+                    class="badge bg-secondary-light">{{ $articles->category->name}}</span>
+            </div>
+            <span
+                class="date">{{$articles->created_at->formatLocalized('%e %b %Y | %H:%M')}}</span>
+        </div>
+        <div class="card-body">
+            <h4>
+                {{ $articles->title}}
+            </h4>
+            <p class="col-md-11">
+                Размышляя об управлении ИТ-активами, я вспомнил один учебный
+                пример.
+                Менеджер по
+                ИТ-мощностям в крупной компании периодически готовил толстенный
+                отчёт
+                руководству. В очередной раз он не принёс отчёт, решив
+                проверить,
+                нужен
+                ли он
+                вообще.
+            </p>
+        </div>
+    </a>
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -43,78 +43,14 @@
             @endif
             @if($i === $left_article)
                 <div class="row">
-                    <div class="col-md-8 flex-column">
-                        <a href="{{ route('articles.show',$articles[$i]->id) }}" class="card flex-grow-1">
-                            <div class="card-head">
-                                <div class="category">
-                                    @if( $articles[$i]->category->parentCategory)
-                                        <span
-                                            class="badge bg-secondary-light">{{ $articles[$i]->category->parentCategory->name}}</span>
-                                    @endif
-                                    <span
-                                        class="badge bg-secondary-light">{{ $articles[$i]->category->name}}</span>
-                                </div>
-                                <span class="date">{{$articles[$i]->created_at->formatLocalized('%e %b %Y | %H:%M')}}</span>
-                            </div>
-                            <div class="card-body">
-                                <h4>
-                                    {{ $articles[$i]->title}}
-                                </h4>
-                                <p class="col-md-11">
-                                    Размышляя об управлении ИТ-активами, я вспомнил один учебный
-                                    пример.
-                                    Менеджер по
-                                    ИТ-мощностям в крупной компании периодически готовил толстенный
-                                    отчёт
-                                    руководству. В очередной раз он не принёс отчёт, решив
-                                    проверить,
-                                    нужен
-                                    ли он
-                                    вообще.
-                                </p>
-                            </div>
-                        </a>
-                    </div>
+                    <x-article-block :articles="$articles[$i]"></x-article-block>
                     <x-category-block link="" category="{{ $articles[$i]->category->name }}"></x-category-block>
-
                 </div>
             @endif
             @if($i === $right_article)
                 <div class="row ">
                     <x-category-block link="" category="{{ $articles[$i]->category->name }}"></x-category-block>
-                    <div class="col-md-8 flex-column">
-                        <a href="{{ route('articles.show',$articles[$i]->id) }}" class="card flex-grow-1">
-                            <div class="card-head">
-                                <div class="category">
-                                    @if( $articles[$i]->category->parentCategory)
-                                        <span
-                                            class="badge bg-secondary-light">{{ $articles[$i]->category->parentCategory->name}}</span>
-                                    @endif
-                                    <span
-                                        class="badge bg-secondary-light">{{ $articles[$i]->category->name}}</span>
-                                </div>
-                                <span
-                                    class="date">{{$articles[$i]->created_at->formatLocalized('%e %b %Y | %H:%M')}}</span>
-                            </div>
-                            <div class="card-body">
-                                <h4>
-                                    {{ $articles[$i]->title}}
-                                </h4>
-                                <p class="col-md-11">
-                                    Размышляя об управлении ИТ-активами, я вспомнил один учебный
-                                    пример.
-                                    Менеджер по
-                                    ИТ-мощностям в крупной компании периодически готовил толстенный
-                                    отчёт
-                                    руководству. В очередной раз он не принёс отчёт, решив
-                                    проверить,
-                                    нужен
-                                    ли он
-                                    вообще.
-                                </p>
-                            </div>
-                        </a>
-                    </div>
+                    <x-article-block :articles="$articles[$i]"></x-article-block>
                 </div>
             @endif
         @endfor
@@ -122,6 +58,5 @@
     <div class="row">
         <a href="{{ route('articles.index')  }}" class="btn btn-secondary">Все статьи</a>
     </div>
-
 
 @endsection
